a42e223da23f3f98a169bd7ef974e7aa4e351083
Emil@DvorakMobile <Emil@DvorakMobile>
2019-03-10 17:49:20 +0100

    Adding proper logging

--- freezer.py
+++ freezer.py
@@ -1,10 +1,15 @@
+# This file is written for Python3.5, which was the default in Raspbian at the time of writing.
+
 from am2320_python.am2320 import AM2320
-from w1thermsensor import W1ThermSensor
+
+import logging
+logger = logging.getLogger(__name__)
 
 try:
     import RPi.GPIO as GPIO
+    from w1thermsensor import W1ThermSensor
 except RuntimeError:
-    print("Not running on a Raspberry Pi, continuing anyway")
+    logger.critical("Not running on a Raspberry Pi, continuing anyway")
     pass
 
 import time
@@ -24,97 +29,91 @@ class Freezer:
 
 
     def __init__(self, COMP_PIN=None):
-        print("Initiating freezer")
-        print("Compressor_Pin: ", COMP_PIN)
+        logger.info("Initiating freezer")
+        logger.info("Compressor_Pin: %s" % COMP_PIN)
         self.COMP_GPIO_PIN = COMP_PIN
         self.get_temperature()
-        print("Temp1: ", self.TEMP1)
-        print("Temp2: ", self.TEMP2)
+        logger.debug("Temp1: %s" % self.TEMP1)
+        logger.debug("Temp2: %s" % self.TEMP2)
 
         # Setup compressor pin:
         try:
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(self.COMP_GPIO_PIN, GPIO.OUT)
         except Exception as e:
-            print("Error setting up GPIO pins")
+            logger.error("Error setting up GPIO pins")
             print(e)
             pass
 
         # Get current compressor state:
         try:
             self.COMP_STATE = GPIO.input(self.COMP_GPIO_PIN)
-            print("compressor is: ", self.COMP_STATE)
+            logger.debug("compressor is: %s" % self.COMP_STATE)
         except:
-            print("Error reading compressor state")
+            logger.error("Error reading compressor state")
             pass
 
-    #@classmethod
+
     def get_temperature(self):
-        print("Getting temperature 1")
+        logger.debug("Getting temperature 1")
         try:
             sensor = AM2320(1)
             (t,h) = sensor.readSensor()
-            #print(t, h)
             self.TEMP1 = round(float(t), 2)
             self.HUMIDITY = round(float(h), 2)
         except FileNotFoundError:
-            print("Could not get i2c device")
+            logger.error("Could not get i2c device")
             pass
         except Exception as e:
-            print("Unknown error getting I2C temperature, ")
+            logger.error("Unknown error getting I2C temperature, ")
             print(e)
             pass
 
-        print("Getting temperature 2")
+        logger.debug("Getting temperature 2")
         try:
             sensor2 = W1ThermSensor()
             self.TEMP2 = round(float(sensor2.get_temperature()), 2)
         except Exception as e:
-            print("Unknown error getting 1-wire temperature, ")
+            logger.error("Unknown error getting 1-wire temperature, ")
             print(e)
             pass
 
         return 0
-    
-    #@classmethod
+
+
     def start(self):
         # Add check here so that we don't try to start the compressor if it's already running
         try:
             if GPIO.input(self.COMP_GPIO_PIN) == 1:
-                print("Compressor is already running")
+                logger.debug("Compressor is already running")
                 return 1
         except Exception as e:
-            print("Got an exception checking compressor state:")
+            logger.critical("Got an exception checking compressor state:")
             print(e)
             pass
 
-        print("Starting Compressor")
+        logger.info("Starting Compressor")
         try:
             GPIO.output(self.COMP_GPIO_PIN, GPIO.HIGH)
             self.COMP_STATE = 1
             self.COMP_ON_TIME = time.time()
         except Exception as e:
-            print("Error starting compressor")
+            logger.critical("Error starting compressor")
             print(e)
             pass
 
-        
-    #@classmethod
+
     def stop(self):
-        print("Stopping Compressor")
+        logger.info("Stopping Compressor")
         if (time.time() - self.COMP_ON_TIME) < 300:
             wait_time = 300 - (time.time() - self.COMP_ON_TIME)
-            print("Compressor started less than 5 minutes ago, wait %d more seconds" % wait_time)
+            logger.warning("Compressor started less than 5 minutes ago, wait %d more seconds" % wait_time)
             return wait_time
         else:
             try:
                 GPIO.output(self.COMP_GPIO_PIN, GPIO.LOW)
+                self.COMP_STATE = 0
+                self.COMP_OFF_TIME = time.time()
             except:
-                print("Error stopping compressor")
+                logger.error("Error stopping compressor")
                 pass
-            self.COMP_STATE = 0
-            self.COMP_OFF_TIME = time.time()
-
-        
-    
-    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This is the main file.
+# This file is written for Python3.5, which was the default in Raspbian at the time of writing.
 
 import sys
 import time
@@ -12,37 +12,57 @@ from freezer import Freezer
 import config
 
 # Setup global stuff:
-#logger = logging.getLogger('freezer')
-#logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('freezer')
+logger.setLevel(logging.getLevelName(config.LOGLEVEL))
+
+# create console handler and add a formatter:
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
 
 ao = appoptics_metrics.connect(config.APPOPTICS_KEY, tags={'ENVIRONMENT': "prod"})
 freezer = Freezer(config.GPIO_PINS["COMP_PIN"])
-
 
 def send_metrics(freezer):
     #####
 
     #### Check here if we have an AppOptics Token, if not, break
-
+    if config.APPOPTICS_KEY == None:
+        # We don't have an AppOptics key, no need to submit metrics
+        return 0
     ####
-    print("Sending metrics")
-    q = ao.new_queue()
-    q.add('freezer.temperature', freezer.TEMP1, tags={'type': 'i2c'}, inherit_tags=True)
-    q.add('freezer.temperature', freezer.TEMP2, tags={'type': '1w'}, inherit_tags=True)
-    q.add('freezer.humidity', freezer.HUMIDITY, tags={'type': 'i2c'}, inherit_tags=True)
-    q.add('freezer.comp_state', freezer.COMP_STATE, inherit_tags=True)
-    if freezer.COMP_STATE == 1:
-        q.add('freezer.comp_state_bool', 1, tags={'state': 'ON'}, inherit_tags=True)
-    else:
-        q.add('freezer.comp_state_bool', 1, tags={'state': 'OFF'}, inherit_tags=True)
-    q.submit()
+    try:
+        print("Sending metrics")
+        q = ao.new_queue()
+        q.add('freezer.temperature', freezer.TEMP1, tags={'type': 'i2c'}, inherit_tags=True)
+        q.add('freezer.temperature', freezer.TEMP2, tags={'type': '1w'}, inherit_tags=True)
+        q.add('freezer.humidity', freezer.HUMIDITY, tags={'type': 'i2c'}, inherit_tags=True)
+        q.add('freezer.comp_state', freezer.COMP_STATE, inherit_tags=True)
+        if freezer.COMP_STATE == 1:
+            q.add('freezer.comp_state_bool', 1, tags={'state': 'ON'}, inherit_tags=True)
+        else:
+            q.add('freezer.comp_state_bool', 1, tags={'state': 'OFF'}, inherit_tags=True)
+        q.submit()
+
+    except appoptics_metrics.exceptions.Unauthorized:
+        logger.error("Wrong AppOptics key, could not submit metrics")
+        pass
+    except Exception as e:
+        logger.error("An ERROR occured while sending metrics to AppOptics")
+        print(e)
+        pass
 
 
 def main(freezer):
-    #print("Starting Main function")
+    logger.debug("Starting Main function")
     freezer.get_temperature()
-    #print("Current temperature 1: %.2f" % freezer.TEMP1)
-    #print("Current temperature 2: %.2f" % freezer.TEMP2)
+    logger.debug("Current temperature 1: %.2f" % freezer.TEMP1)
+    logger.debug("Current temperature 2: %.2f" % freezer.TEMP2)
 
     if freezer.TEMP1 > config.MAX_TEMP:
         # Temperature is to high:
@@ -52,32 +72,14 @@ def main(freezer):
         freezer.stop()
     else:
         # Will this ever happen?
-        print("Temp OK")
+        logger.debug("Temp OK")
 
     # Just print a newline:
     #print()
 
-while 1:
-    # Control the freezer:
-    main(freezer)
-
-
-    # Send metrics:
-    ### Add check if we have an AppOptics key here
-    ### Stop otherwise
-    try:
+if __name__ == '__main__':
+    while 1:
+        # Control the freezer:
+        main(freezer)
         send_metrics(freezer)
-    except appoptics_metrics.exceptions.Unauthorized:
-        print("ERROR: Wrong AppOptics key, could not submit metrics")
-        pass
-    except Exception as e:
-        print("An ERROR occured while sending metrics to AppOptics")
-        print(e)
-        pass
-
-#### Debug:
-#    freezer.CURRENT_TEMP += 0.05
-#    if freezer.COMP_STATE == 1:
-#        freezer.CURRENT_TEMP -= 0.1
-#    print()
-    time.sleep(60)
+        time.sleep(60)
